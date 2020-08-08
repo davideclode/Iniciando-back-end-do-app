ac9bba9560daf65c3638f90f02fa08bef852d1b6
Davide da Silva <silvada1@hotmail.com>
2020-08-08 02:24:45 -0300

    chore: Data base, User registration, Authentication and File uploaded

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 // Temos que importar o CreateUsersService.ts aqui
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
+// Agora, o "upload" é uma instancia do "multer". Dentro dele, temos alguns métodos como: any, none, fields. upload.single significa que vou fazer upload de um único arquivo.
 
 // // Essa parte foi adicionada depois de "routes.use('/appointments', appointmentsRouter);" do arquivo index.ts
 // // Agora, se formos criar uma rota para criar um agendamento(uma rota do tipo post), não precisamos colocar "/appointments". Podemos escrever simplesmente "/". Isto porque utilizamos "routes.use"
@@ -34,5 +40,16 @@ usersRouter.post('/', async (request, response) => {
   }
 });
 
+// Estamos utilizando "patch" porque queremos atualizar a penas uma informação específica do usuário
+// Vamos precisar que o usuário esteja autenticado. Para isso importamos o "ensureAuthenticated";
+// Depois de verificar que o usuário foi autenticado, vamos colocar mais um middleware "upload.single('Nome do campo que conter a imagem quando eu chamar esta rota')".
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 // // Vamos exportar a variável appointementesRouter
 export default usersRouter;
